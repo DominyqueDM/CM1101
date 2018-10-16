db3991484a342f1867929148b0d9f00ac63810ba
DOMINYQUEDM <mohammeddd@cardiff.ac.uk>
2018-10-16 09:52:35 +0100

    yay, i committed my first file!

---
+++ hello.py
@@ -0,0 +1,10 @@
+ Volume in drive H is COMSCH
+ Volume Serial Number is 5C43-4F4D
+
+ Directory of H:\repos\CM1101
+
+16/10/2018  09:10    <DIR>          .
+16/10/2018  09:10    <DIR>          ..
+16/10/2018  09:47               140 hello.py
+               1 File(s)            140 bytes
+               2 Dir(s)  418,171,944,960 bytes free
